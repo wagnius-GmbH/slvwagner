e5df37b3716d038c8e7527d6e4d54d2c9e887fe6
Florian Wagner <slvwagner@gmail.com>
2021-09-28 20:48:30 +0200

    Function update: get_NPS Levels can now be defined by df_levels argument

--- R/marketing.R
+++ R/marketing.R
@@ -2,11 +2,15 @@
 #' Get Net Promoter Score
 #'
 #' @name get_NPS
-#' @description Get Net Promoter Score of rating 1...10. NPS = Ratio Promoters - Ratio Detractors,
+#' @description Calculates the Net Promoter Score.
+#' @details NPS = Ratio Promoters - Ratio Detractors,
 #' whereas the Ratio of Promoters is calculated by count of Promoters / count total
 #' and the the Ratio of Detractors is calculated by count of Detractors / count total.
-#' @param rating string with column name of data frame containing the vector of the ratings
-#' @param c_group string with column name to group the data frame and calcultae NPS for each group
+#' If the \code{c_group} parameter is available the NPS score will be calculated for each group.
+#' If the \code{df_levels} parameter is provided the level to find Promoters, Passives and Detractors can be defined by this definition.
+#' @param rating string with column name of the rating.
+#' @param c_group string with the column name to group the data frame.
+#' @param df_levels data frame containing the rating range for each type (Promoters, Passives, Detractors).
 #' @examples
 #' library(tidyverse)
 #' # Test data
@@ -17,15 +21,29 @@
 #'
 #' get_NPS(df_data, "Feedback", "Gruppe")
 #' get_NPS(df_data, "Feedback")
+#'
+#' df_levels <- tibble(Detractors= c(1,3),Passives= c(4,4), Promoters= c(5,5))
+#' df_data <- tibble(user   = c(letters[1:10],rep("a",5)),
+#'                   Feedback = c(1:5,rep(5,10)),
+#'                   Gruppe = c(rep("A",10),rep("B",5)))
+#' get_NPS(df_data, "Feedback", "Gruppe",df_levels)
+#' get_NPS(df_data, "Feedback",df_levels = df_levels)
 #' @export
 
-get_NPS <- function(x, rating , c_group ) {
+get_NPS <- function(x, rating , c_group , df_levels) {
+  # Levels NPS
+  if(missing(df_levels)) {
+    df_levels <- tibble(Detractors = c(1,6),
+                       Passives   = c(7,8),
+                       Promoters  = c(9,10))
+  }
   # exit function call if rating is not defined
   if(missing(rating)){
     message("function error get_NPS(): \nrating argument is missing")
   }else{
     #copy data
     df_data <- x
+
     # check if c_groupe is availabel
     if(missing(c_group)){
       #change names of data frame
@@ -33,13 +51,14 @@ get_NPS <- function(x, rating , c_group ) {
         mutate(name_converted = if_else(name==rating,"rating", name))%>%
         select(name_converted)%>%
         pull()
+
       # Create feature Promoters, Passives and Detractors
       df_data <- df_data%>%
-        mutate(Detractors = between(paste(rating), 0, 6)%>%
+        mutate(Detractors = between(paste(rating), df_levels$Detractors[1], df_levels$Detractors[2])%>%
                  if_else("Detractors","x_x")%>%na_if("x_x"),
-               Passives   = between(paste(rating), 7, 8)%>%
+               Passives   = between(paste(rating), df_levels$Passives[1], df_levels$Passives[2])%>%
                  if_else("Passives"  ,"x_x")%>%na_if("x_x"),
-               Promoters  = between(paste(rating), 9,10)%>%
+               Promoters  = between(paste(rating), df_levels$Promoters[1],df_levels$Promoters[2])%>%
                  if_else("Promoters" ,"x_x")%>%na_if("x_x")
         )
       # Merge to single feature
@@ -69,11 +88,11 @@ get_NPS <- function(x, rating , c_group ) {
         pull()
       # Create feature Promoters, Passives and Detractors
       df_data <- df_data%>%
-        mutate(Detractors = between(paste(rating), 0, 6)%>%
+        mutate(Detractors = between(paste(rating), df_levels$Detractors[1], df_levels$Detractors[2])%>%
                  if_else("Detractors","x_x")%>%na_if("x_x"),
-               Passives   = between(paste(rating), 7, 8)%>%
+               Passives   = between(paste(rating), df_levels$Passives[1], df_levels$Passives[2])%>%
                  if_else("Passives"  ,"x_x")%>%na_if("x_x"),
-               Promoters  = between(paste(rating), 9,10)%>%
+               Promoters  = between(paste(rating), df_levels$Promoters[1],df_levels$Promoters[2])%>%
                  if_else("Promoters" ,"x_x")%>%na_if("x_x")
         )
       # Merge to single feature
