0d19bc5853ebb77bc9337fe8c66bc6e2053689e6
Florian Wagner <slvwagner@gmail.com>
2022-01-05 14:48:00 +0100

    New functions: cas_rot_matrix2d, cas_rot_matrix3d

--- R/math.R
+++ R/math.R
@@ -366,7 +366,7 @@ math_slerp <- function(R,x1,x2,cp,nb_points = 10) { #slerp aus drei Punkten, Rad
 }
 
 #######################################
-#' 2d rotation matrix
+#' CAS 2d rotation matrix
 #'
 #' @name math_rot_matrix2d
 #' @description
@@ -383,11 +383,39 @@ math_slerp <- function(R,x1,x2,cp,nb_points = 10) { #slerp aus drei Punkten, Rad
 #' math_rot_matrix2d(c(-pi))
 #' @export
 
-math_rot_matrix2d <- function(x){
+cas_rot_matrix2d <- function(x){
   #Drehmatrix 2D
   matrix(c(cos(x),sin(x),-sin(x),cos(x)), ncol = 2)
 }
 
+#######################################
+#' 2d rotation matrix
+#'
+#' @name cas_rot_matrix2d
+#' @description
+#' Calculates a 2D rotation matrix with a given angle \code{x}
+#' @details
+#' The calculation is done by Ryacas (yacas) and uses symbolic math. The package Ryacas and the software YACAS \url{http://www.yacas.org/}
+#' needs to be installed.
+#'
+#' @param  x vector of an angel in radiant
+#' @author Florian Wagner
+#' \email{florian.wagner@wagnius.ch}
+#' @returns
+#' Returns rotation matrix for given angle
+#' @examples
+#' library(Ryacas)
+#' cas_rot_matrix2d(c(pi))
+#' cas_rot_matrix2d(c(-pi)|>ysym())
+#' @export
+
+cas_rot_matrix2d <- function(x){
+  if(is.list(x)){
+    y_cos <- cos(x)
+    y_sin <- sin(x)
+    yac_str(paste0("{{",y_cos,",",-y_sin,"},{",y_sin,",",y_cos,"}}"))
+  }else print("No yacs object supplied")
+}
 
 
 #######################################
@@ -422,6 +450,51 @@ math_rot_matrix3d <- function(x,angle){
              x[3]^2*(1-cos(angle))+cos(angle))),
          ncol = 3, byrow = TRUE)
 }
+
+#######################################
+#' CAS 3d rotation matrix
+#'
+#' @name cas_rot_matrix3d
+#' @description
+#' Calculates a 3D rotation matrix from a given axis and angle. The axis is defined by \code{x|>ysym()}. The Rotation will be done around the axis defined by
+#' \code{angle|>ysym()} and the root. The angle will be appied by the right hand rule.
+#' @details
+#' The calculation is done by Ryacas (yacas) and uses symbolic math. The package Ryacas and the software YACAS \url{http://www.yacas.org/}
+#' needs to be installed.
+#'
+#' @param  x unit vector c(x1,x2,x3)|>ysym()
+#' @param  angle vector of angel in radiant c(angle)|>ysym()
+#' @author Florian Wagner
+#' \email{florian.wagner@wagnius.ch}
+#' @returns
+#' Returns 3D rotation matrix for given axis and angle.
+#' @examples
+#' library(Ryacas)
+#' cas_rot_matrix3d(matrix(c(1,2,3)),c(-pi))
+#' cas_rot_matrix3d(matrix(c(1,2,3))|>ysym(),c(-pi)|>ysym())
+#' @export
+
+cas_rot_matrix3d <- function(x,angle){
+  if(is.list(x) & is.list(angle)){
+    y_cos <- cos(angle)
+    cos1_cos <- yac_str(paste0("(",1,"-",y_cos,")"))
+    y_sin <- sin(angle)
+    yac_str(paste0(
+      "{{",x[1],"^2       *(",cos1_cos,")+",y_cos,",
+    ",x[1],"*",x[2],"*(",cos1_cos,")-",x[3],"*",y_sin,",
+    ",x[1],"*",x[3],"*(",cos1_cos,")+",x[2],"*",y_sin,"},
+
+    {",x[2],"*",x[1],"*(",cos1_cos,")+",x[3],"*",y_sin,",
+     ",x[2],"^2       *(",cos1_cos,")+",y_cos,",
+     ",x[2],"*",x[3],"*(",cos1_cos,")-",x[1],"*",y_sin,"},
+
+   {",x[3],"*",x[1],"*(",1,"-",y_cos ,")-",x[2],"*",y_sin,",
+    ",x[3],"*",x[2],"*(",1,"-",y_cos ,")+",x[1],"*",y_sin,",
+    ",x[3],"^2       *(",1,"-",y_cos ,")+",y_cos,"}}"))
+  }else print("No yacs object supplied")
+}
+
+
 
 #######################################
 #' Transform vector by a rotation matrix
