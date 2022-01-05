50563ea5cc997575d8cf46b7401c4e872f198384
Florian Wagner <slvwagner@gmail.com>
2022-01-05 13:56:33 +0100

    Removed function: math_rot_matrix_basic

--- R/math.R
+++ R/math.R
@@ -389,44 +389,6 @@ math_rot_matrix2d <- function(x){
 }
 
 
-#######################################
-#' Basic rotation matrix
-#'
-#' @name math_rot_matrix_basic
-#' @description
-#' Calculates a 3D rotation matrix from a given principle axis and a given angle. The axis is defined by \code{ax}. The Rotation will be done around the axis defined by
-#' \code{ax} and the root.
-#' @details
-#' \url{https://en.wikipedia.org/wiki/Rotation_matrix}
-#' @param x angel in radiant
-#' @param ax parameter to define which principle axis will be used to calculate the rotation matrix
-#' @author Florian Wagner
-#' \email{florian.wagner@wagnius.ch}
-#' @returns
-#' Returns 3D basic rotation matrix for given axis and angle.
-#' @examples
-#' math_rot_matrix_basic(pi, "x")
-#' math_rot_matrix_basic(pi, "x2")
-#' math_rot_matrix_basic(pi, 3)
-#' @export
-
-math_rot_matrix_basic <- function(x,ax){
-  if(is.character(ax)){
-    ax <- tolower(ax)
-  }
-  if(ax == "x" | ax == "x1" | ax == 1 ){
-    #Axis x or the fist axis x1
-    matrix(c(1,0,0,0,cos(x),sin(x),0,-sin(x),cos(x)), ncol = 3)
-  }else if(ax == "y" | ax == "x2" | ax == 2){
-    #Axis x or the fist axis x1
-    matrix(c(cos(x),0,sin(x),0,1,0,-sin(x),0,cos(x)), ncol = 3)
-  }else if(ax == "z" | ax == "x3" | ax == 3){
-    #Axis x or the fist axis x1
-    matrix(c(cos(x),-sin(x),0,sin(x),cos(x),0,0,0,1), ncol = 3)
-  }else{writeLines(paste0("Parameter ax is not according to following fromats:",
-                          "\ncharacter; c(\"x\",\"y\",\"z\") or c(\"x1\",\"x2\",\"x3\") nor integer; c(1,2,3) to specify the rotation axis.")
-  )}
-}
 
 #######################################
 #' 3d rotation matrix
