41b2e47393e1192f0f06517eba76adbb9914b787
Florian Wagner <slvwagner@gmail.com>
2022-03-21 18:37:54 +0100

    New function: calculate_cpk

--- R/stat.R
+++ R/stat.R
@@ -16,3 +16,31 @@
 standard_norm_dist <- function(x,mu,sd){
   1/sqrt(2*pi*sd)*exp(-(x-mu)^2/2*sd)
 }
+
+#######################################
+#' Process capability index
+#'
+#' @name calculate_cpk
+#' @description Calculation of Cpk Process capability index given parameter USL, LSL mean \eqn{\mu} and standard deviation \eqn{\sigma}.
+#' @details Estimates what the process is capable of producing, considering that the process mean may not be centered between the specification (Upper and Lower) limits.
+#' @details <https://en.wikipedia.org/wiki/Process_capability_index>
+#' @param LSL lower specification limit parameter
+#' @param USL upper specification limit parameter
+#' @param mue the estimated mean of the process \eqn{\mu}
+#' @param sigma standard deviation \eqn{\sigma}
+#' @author Florian Wagner
+#' \email{florian.wagner@wagnius.ch}
+#' @returns vector
+#' @examples calculate_cpk(LSL = -0.03, USL = -0.01, mue = -0.02, sigma = 0.005)
+#' @examples calculate_cpk(LSL = -0.05, USL = 0.05, mue =c(-0.01, -0.015), sigma = c(0.001,0.015))
+#' @export
+calculate_cpk <- function(LSL, USL, mue, sigma){
+  if(length(USL)==1 & is.numeric(USL) & length(LSL)==1 & is.numeric(LSL)){
+    return(min(mue-LSL, USL-mue)/(3*sigma))
+  }else{
+    writeLines("vector LSL and USL have grater lenght than 1")
+    NULL
+  }
+}
+
+
