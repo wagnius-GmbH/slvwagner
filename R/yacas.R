8f340303a59ac12e8ad528d1afb6f413cfb39ed3
Florian Wagner <slvwagner@gmail.com>
2022-03-30 18:17:45 +0200

    Fuction moved to cas file

--- R/yacas.R
+++ R/yacas.R
@@ -1,4 +1,36 @@
 #######################################
+#' CAS 2d rotation matrix
+#'
+#' @name cas_rot_matrix2d
+#' @description
+#' Calculates a 2D rotation matrix with a given angle \code{x}
+#' @details
+#' The calculation is done by Ryacas (yacas) and uses symbolic math. The package Ryacas and the software YACAS \url{http://www.yacas.org/}
+#' needs to be installed.
+#'
+#' @param  x vector of an angel in radiant
+#' @author Florian Wagner
+#' \email{florian.wagner@wagnius.ch}
+#' @returns
+#' Returns rotation matrix for given angle
+#' @examples
+#' library(Ryacas)
+#' cas_rot_matrix2d(c(pi))
+#' rot_matrix <- cas_rot_matrix2d(c(-pi)|>ysym())
+#' rot_matrix
+#' @export
+
+cas_rot_matrix2d <- function(x){
+  if(is.list(x)){
+    y_cos <- cos(x)
+    y_sin <- sin(x)
+    yac_str(paste0("{{",y_cos,",",-y_sin,"},{",y_sin,",",y_cos,"}}"))|>
+      ysym()
+  }else print("No yacs object supplied")
+}
+
+
+#######################################
 #' CAS 3d rotation matrix
 #'
 #' @name cas_rot_matrix3d
@@ -16,7 +48,6 @@
 #' Returns 3D rotation matrix for given axis and angle.
 #' @examples
 #' library(Ryacas)
-#' cas_rot_matrix3d(matrix(c(1,2,3)),c(-pi))
 #' rot_matrix <- cas_rot_matrix3d(matrix(c(1,2,3))|>ysym(),c(-pi)|>ysym())
 #' rot_matrix
 #' @export
