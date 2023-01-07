de0c94887a1547f461bdccdc3942343cf822e895
Florian Wagner <slvwagner@gmail.com>
2023-01-07 14:27:23 +0100

    Documentation update: Signal_center

--- R/singal.R
+++ R/singal.R
@@ -8,9 +8,18 @@
 #' \email{florian.wagner@wagnius.ch}
 #' @returns centred vector \code{x}
 #' @examples
-#' x <- rnorm(100,mean = -0.5)
+#' x <- rnorm(100,mean = -2.5)
 #' plot(x)
 #' signal_center(x)|>plot()
+#' library(tidyverse)
+#' tibble(y = rnorm(100,mean = -2.5))|>
+#'   mutate(x = row_number(y),
+#'          y_centered = signal_center(y))|>
+#' pivot_longer(cols = c("y", "y_centered"))|>
+#' ggplot(aes(x,value, color = name))+
+#'   geom_point()+
+#'   geom_hline(yintercept = 0)
+
 #' @export
 
 signal_center <- function (x)
@@ -21,3 +30,4 @@ signal_center <- function (x)
   else if(c_center == 0) return(x)
   else return(x+abs(c_center))
 }
+
