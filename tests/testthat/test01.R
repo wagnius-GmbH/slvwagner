fdae64672282abf2c09921a4ff1c4b920eb30ec5
Florian Wagner <slvwagner@gmail.com>
2023-01-07 14:53:31 +0100

    New test for singal_center function

--- tests/testthat/test01.R
+++ tests/testthat/test01.R
@@ -2,3 +2,9 @@ test_that("math_betrag",{
   expect_equal(math_betrag(c(1,1)), sqrt(2))
   expect_equal(math_betrag(c(1,1,1)), sqrt(3))
 })
+
+test_that("signal_center",{
+  x <- rnorm(100,-2.5)|>signal_center()
+  c_range = range(x)|>abs()
+  expect_equal(c_range[1],c_range[2])
+})
