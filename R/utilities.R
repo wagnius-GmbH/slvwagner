9dffd8dbf7c74c87b5a9ebd586732b077fbdc8eb
Florian Wagner <slvwagner@gmail.com>
2023-03-14 20:06:11 +0100

    reworked r_ functions

--- R/utilities.R
+++ R/utilities.R
@@ -9,14 +9,14 @@
 #' @examples # Copy file path to the cliboard and call the function.
 #' utils::writeClipboard("C:\\Windows")
 #' r_path()
+#' r_path("C:\\Windows")
 #' @export
 
 r_path <- function(path = "clipboard") {
-  y <- if (path == "clipboard") {
-    utils::readClipboard()
+  if (path == "clipboard") {
+    y <- utils::readClipboard()
   } else {
-    cat("Please enter the path:\n\n")
-    readline()
+    y <- path
   }
   x <- chartr("\\", "/", y)
   utils::writeClipboard(x)
@@ -28,18 +28,24 @@ r_path <- function(path = "clipboard") {
 #'
 #' @name r_win_path
 #' @description Function to convert R path to windows compatible path.
-#' @param  x R path character vector
+#' @param  path R path character vector
 #' @return Windows compatible character vector
 #' @examples
 #' r_win_path(tempdir())
 #' @export
 
-r_win_path <- function(x){
-   x <- chartr("\\", "/", x)
-   utils::writeClipboard(x)
-   return(x)
+r_win_path <- function(path = "clipboard"){
+  if (path == "clipboard") {
+    y <- utils::readClipboard()
+  }else{
+    y <- path
+  }
+  x <- chartr("/","\\", y)
+  utils::writeClipboard(x)
+  return(x)
 }
 
+# r_path("C:\\src\\repos\\slvwagner")
 
 ###################################################################
 #' Check if variable is defined in R`s global environment.
@@ -67,15 +73,15 @@ r_is.defined <- function(sym) {
 #' purple, red, white, yellow
 #'
 #' @param text character vector
-#' @param fg foreground colour, defaults to white
-#' @param bg background colour, defaults to transparent
+#' @param color foreground colour, defaults to white
 #' @examples
-#' print(r_colourise("Red", "red"))
-#' cat(r_colourise("Red", "red"), "\n")
-#' cat(r_colourise("White on red", "white", "red"), "\n")
+#' # copy paste the code to the terminal
+#' print(r_colourise("text", "red"))
+#' r_colourise("text", "red")
+#' c("this", "text","is colored")|>r_colourise(c("red","green","yellow"))
 #' @export
 
-r_colourise <- function(text, fg = "black", bg = NULL) {
+r_colourise <- function(text, color = "black") {
   term <- Sys.getenv()["TERM"]
   colour_terms <- c("xterm-color","xterm-256color", "screen", "screen-256color")
 
@@ -86,46 +92,35 @@ r_colourise <- function(text, fg = "black", bg = NULL) {
   col_escape <- function(col) {
     paste0("\033[", col, "m")
   }
-
-  col <- .fg_colours[tolower(fg)]
-  if (!is.null(bg)) {
-    col <- paste0(col, .bg_colours[tolower(bg)], sep = ";")
-  }
-
+  # stings to concatenate
+  col <- .color_colours[tolower(color)]
   init <- col_escape(col)
   reset <- col_escape("0")
-  paste0(init, text, reset)
+
+  # execute it
+  paste0(init, text, reset)|>
+    writeLines()
 }
 
-.fg_colours <- c(
+.color_colours <- c(
   "black" = "0;30",
-  "blue" = "0;34",
-  "green" = "0;32",
-  "cyan" = "0;36",
-  "red" = "0;31",
-  "purple" = "0;35",
-  "brown" = "0;33",
-  "light gray" = "0;37",
-  "dark gray" = "1;30",
-  "light blue" = "1;34",
-  "light green" = "1;32",
-  "light cyan" = "1;36",
-  "light red" = "1;31",
-  "light purple" = "1;35",
-  "yellow" = "1;33",
-  "white" = "1;37"
+          "blue" = "0;34",
+          "green" = "0;32",
+          "cyan" = "0;36",
+          "red" = "0;31",
+          "purple" = "0;35",
+          "brown" = "0;33",
+          "lightgray" = "0;37",
+          "darkgray" = "1;30",
+          "lightblue" = "1;34",
+          "lightgreen" = "1;32",
+          "lightcyan" = "1;36",
+          "pink" = "1;31",
+          "purple" = "1;35",
+          "yellow" = "1;33",
+          "white" = "1;37"
 )
 
-.bg_colours <- c(
-  "black" = "40",
-  "red" = "41",
-  "green" = "42",
-  "brown" = "43",
-  "blue" = "44",
-  "purple" = "45",
-  "cyan" = "46",
-  "light gray" = "47"
-)
 
 r_cmd_running <- function() {
   nchar(Sys.getenv('R_TESTS')) != 0
