98ade79325e13aa7c04f502b53a93cab57e4b32e
Florian Wagner <slvwagner@gmail.com>
2023-05-23 13:30:15 +0200

    Updated documentation for r_signif

--- R/helper.R
+++ R/helper.R
@@ -2,22 +2,23 @@
 #' Format number to defined signify(x,digits = 3)
 #'
 #' @name r_signif
-#' @description creates the same result as the Base R function format(\code{x}, format = "g", digits = 3), it`s just a wrapper with my preferred parameters.
+#' @description
+#' Formatting a numerical vector \code{x} to a character vector. The format will be defined with respect to the significant digits for each vector index \code{x[]}.
+#' So the string length of an vector index may vary according to the max(\code{x}) and min(\code{x}) supplied.
+#' @details
+#'  creates the same result as the Base R function format(\code{x}, format = "g", digits = 3), it`s just a wrapper with my preferred parameters.
 #' @param x numerical vector
 #' @param significant_digits number of significant digits you prefer.
 #' @author Florian Wagner
 #' \email{florian.wagner@wagnius.ch}
 #' @returns character vector
 #' @examples
-#' library(stringi)
+#' set.seed(123456)
 #' test <- c(25,signif(rnorm(10),3))
-#' test|>  paste0("\n")|>writeLines()
-#' test|>
-#'   r_signif()|>
-#'   paste0("\n")|>writeLines()
-#' test|>
-#'   format(format = "g", digits = 3)|>
-#'   paste0("\n")|>writeLines()
+#' test|>paste0()|>writeLines()
+#' test|>r_signif()|>writeLines()
+#' test|>format(format = "g", digits = 3)|>writeLines()
+#' c(25,9255666,0.0000034646465321)|>r_signif()|>writeLines()
 
 #' @export
 
