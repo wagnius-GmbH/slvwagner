644bc2375493fd591bf6441a7e76183f7b6e5c7b
Florian Wagner <slvwagner@gmail.com>
2023-09-06 20:26:54 +0200

    update function r_tbl_contents: nubering the heading is now avialable

--- R/Rmarkdown_utiilities.R
+++ R/Rmarkdown_utiilities.R
@@ -4,9 +4,9 @@
 #' @description
 #' Scans documents for headings and creates a table of contents (hyper linked). The returned string can directly be written as .Rmd file.
 #' @details
-#' The function argument is a stirng of a R markdown .Rmd file which can be loaded via \code{readLines("fileName.Rmd")}
-#'
+#' The function argument is a string of a R markdown .Rmd file which can be loaded via \code{readLines("fileName.Rmd")}
 #' @param  c_Rmd Rmd file string
+#' @param  TOC_nb boolean to enable numbering for each heading
 #' @return .Rmd file string
 #' @examples
 #' print(tbl_of_contents.Rmd)
@@ -14,29 +14,30 @@
 #' c_rmd|>write("test.Rmd")
 #' rmarkdown::render("test.Rmd", c("html_document"))
 #' browseURL("test.html")
+#' c_rmd <- r_tbl_contents(tbl_of_contents.Rmd, T)
+#' c_rmd|>write("test.Rmd")
+#' rmarkdown::render("test.Rmd", c("html_document"))
+#' browseURL("test.html")
 #' @export
 
-r_tbl_contents <- function(c_Rmd) {
-  # regex search pattern
-  p <- "^#"
-
-  # find structure
-  c_struct <- c_Rmd[stringr::str_detect(c_Rmd, p)]
-  c_struct
-
+r_tbl_contents <- function(c_Rmd, TOC_nb = FALSE) {
   # table of contents
-  tbl_contents <- c_struct|>stringr::str_remove_all("#")|>stringr::str_split("<", simplify = T)|>stringr::str_trim()
-  # create links
-  tbl_contents <- paste0("[",tbl_contents,"]","(#",tbl_contents,")")
+  headings <- c_Rmd[stringr::str_detect(c_Rmd, "#")]|>stringr::str_remove_all("#")|>stringr::str_trim()
+  headings
 
-  # search string
-  c_detect <- c("^#\\s",
-                "^##\\s",
-                "^###\\s",
-                "^####\\s",
-                "^#####\\s",
-                "^######\\s"
+  # Hello
+  df_data <- data.frame(index = 1:length(c_Rmd),
+                        c_Rmd,
+                        is.heading = stringr::str_detect(c_Rmd, "#")
   )
+
+  df_data$c_add <- rep("",nrow(df_data))
+  df_data$`#` <- stringr::str_detect(df_data$c_Rmd, "^#\\s")|>ifelse(1,0)
+  df_data$`##` <- stringr::str_detect(df_data$c_Rmd, "^##\\s")|>ifelse(1,0)
+  df_data$`###` <- stringr::str_detect(df_data$c_Rmd, "^###\\s")|>ifelse(1,0)
+  df_data$`####` <- stringr::str_detect(df_data$c_Rmd, "^####\\s")|>ifelse(1,0)
+  df_data$`#####` <- stringr::str_detect(df_data$c_Rmd, "^#####\\s")|>ifelse(1,0)
+  df_data$`######` <- stringr::str_detect(df_data$c_Rmd, "^######\\s")|>ifelse(1,0)
 
   # Structure string
   c_add <- c("* ",
@@ -46,48 +47,110 @@ r_tbl_contents <- function(c_Rmd) {
              "                + ",
              "                    + "
   )
+  heading_cnt <- rep(0,6)
+  heading_cnt
 
-  # create table of contents
-  # first_heading <- TRUE
-  for (ii in 1:length(tbl_contents)) {
-    if(stringr::str_detect(c_struct[ii],c_detect[1]) ){
-      tbl_contents[ii] <- paste0(c_add[1],tbl_contents[ii])
-      # first_heading <- FALSE
+  # loop through document and check for heading
+  for (ii in 1:nrow(df_data)) {
+    if(df_data$`#`[ii]){
+      df_data$`#`[ii] <- heading_cnt[1] + 1
+      heading_cnt[1] <- heading_cnt[1] + 1
+      df_data$c_add[ii] <- c_add[1]
     }
-    else if(stringr::str_detect(c_struct[ii],c_detect[2])){
-      tbl_contents[ii] <- paste0(c_add[2],tbl_contents[ii])
+    else if(df_data$`##`[ii]){
+      df_data$`##`[ii] <- heading_cnt[2] + 1
+      df_data$`#`[ii] <- heading_cnt[1]
+      heading_cnt[2] <- heading_cnt[2] + 1
+      df_data$c_add[ii] <- c_add[2]
     }
-    else if(stringr::str_detect(c_struct[ii],c_detect[3])){
-      tbl_contents[ii] <- paste0(c_add[3],tbl_contents[ii])
+    else if(df_data$`###`[ii]){
+      df_data$`###`[ii] <- heading_cnt[3] + 1
+      df_data$`##`[ii] <- heading_cnt[1]
+      df_data$`#`[ii] <- heading_cnt[1]
+      heading_cnt[3] <- heading_cnt[3] + 1
+      df_data$c_add[ii] <- c_add[3]
     }
-    else if(stringr::str_detect(c_struct[ii],c_detect[4])){
-      tbl_contents[ii] <- paste0(c_add[4],tbl_contents[ii])
+    else if(df_data$`####`[ii]){
+      df_data$`####`[ii] <- heading_cnt[4] + 1
+      df_data$`###`[ii] <- heading_cnt[1]
+      df_data$`##`[ii] <- heading_cnt[1]
+      df_data$`#`[ii] <- heading_cnt[1]
+      heading_cnt[4] <- heading_cnt[4] + 1
+      df_data$c_add[ii] <- c_add[4]
     }
-    else if(stringr::str_detect(c_struct[ii],c_detect[5])){
-      tbl_contents[ii] <- paste0(c_add[5],tbl_contents[ii])
+    else if(df_data$`#####`[ii]){
+      df_data$`#####`[ii] <- heading_cnt[5] + 1
+      df_data$`####`[ii] <- heading_cnt[1]
+      df_data$`###`[ii] <- heading_cnt[1]
+      df_data$`##`[ii] <- heading_cnt[1]
+      df_data$`#`[ii] <- heading_cnt[1]
+      heading_cnt[5] <- heading_cnt[5] + 1
+      df_data$c_add[ii] <- c_add[5]
     }
-    else if(stringr::str_detect(c_struct[ii],c_detect[6])){
-      tbl_contents[ii] <- paste0(c_add[6],tbl_contents[ii])
+    else if(df_data$`######`[ii]){
+      df_data$`######`[ii] <- heading_cnt[6] + 1
+      df_data$`#####`[ii] <- heading_cnt[1]
+      df_data$`####`[ii] <- heading_cnt[1]
+      df_data$`###`[ii] <- heading_cnt[1]
+      df_data$`##`[ii] <- heading_cnt[1]
+      df_data$`#`[ii] <- heading_cnt[1]
+      heading_cnt[6] <- heading_cnt[6] + 1
+      df_data$c_add[ii] <- c_add[6]
     }
   }
 
-  # put anchor to all headings
-  c_Rmd <- ifelse(stringr::str_detect(c_Rmd, p), paste0(c_Rmd, "<a name=\"",c_Rmd|>stringr::str_remove_all("#")|>stringr::str_trim(),"\"></a>"), c_Rmd)
-  c_Rmd
+  # Create number string
+  df_data$heading_nb <- df_data[,5:ncol(df_data)]|>
+    apply(1,function(x){
+      x <- ifelse(x == 0,NA,x)
+      paste0(x[!is.na(x)], collapse = ".")
+    })
+
+  df_data$c_Rmd_ <- ifelse(df_data$is.heading,
+                           paste0(df_data$c_Rmd," " ,df_data$heading_nb),
+                           c_Rmd
+  )
+
+  # TOC without numbering
+  df_data$TOC <- ifelse(df_data$is.heading,
+                        paste0(df_data$c_add,"[",headings,"](#",headings,")"),
+                        "")
+  # TOC_ with numbering
+  df_data$TOC_ <- ifelse(df_data$is.heading,
+                         paste0(df_data$c_add,"[",headings," ",df_data$heading_nb,"](#",headings," ",df_data$heading_nb,")"),
+                         "")
+  cbind(df_data$c_Rmd,df_data$c_Rmd_)
+
+  # create TOC string
+  c_TOC <- df_data$TOC[df_data$is.heading]|>
+    paste0("\n", collapse = "")
+
+  # create TOC string
+  c_TOC_ <- df_data$TOC_[df_data$is.heading]|>
+    paste0("\n", collapse = "")
 
   # find position to insert table of contents
   for(ii in 1:length(c_Rmd)){
-    if(stringr::str_detect(c_Rmd[ii],p)) {
+    if(stringr::str_detect(c_Rmd[ii],"#")) {
       c_start <- ii
       break
     }
   }
-
-  # insert table off contents
-  c_Rmd <- c(c_Rmd[1:(c_start-1)],
-             "\n# Tabel of Content",
-             tbl_contents, "\n",
-             c_Rmd[c_start:length(c_Rmd)])
-  return(c_Rmd)
+  if(!TOC_nb){
+    # insert table off contents
+    c_Rmd <- c(df_data$c_Rmd[1:(c_start - 1)],
+               "# Tabel of Content",
+               c_TOC,
+               "\n",
+               df_data$c_Rmd[c_start:nrow(df_data)])
+    return(c_Rmd)
+  }else{
+    # insert table off contents
+    c_Rmd <- c(df_data$c_Rmd_[1:(c_start - 1)],
+               "# Tabel of Content",
+               c_TOC_,
+               "\n",
+               df_data$c_Rmd_[c_start:nrow(df_data)])
+    return(c_Rmd)
+  }
 }
-
--- R/data.R
+++ R/data.R
@@ -4,11 +4,10 @@
 #' @description
 #' Data for r_tbl_contents
 #' @export
-
 tbl_of_contents.Rmd <- function(){}
 
-#tbl_of_contents.Rmd <- readLines(paste0(getwd(),"/files/input/tbl_of_contents.Rmd"))
-#usethis:::use_data(tbl_of_contents.Rmd, internal = TRUE, overwrite = TRUE)
+# tbl_of_contents.Rmd <- readLines(paste0(getwd(),"/files/input/tbl_of_contents.Rmd"))
+# usethis:::use_data(tbl_of_contents.Rmd, internal = TRUE, overwrite = TRUE)
 
 ###################################################################
 #' Automatically creates a table of contents for a .Rmd file
