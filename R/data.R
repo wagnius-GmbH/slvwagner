3f54db164d64491900969b4b7b291d4d6a32aa8f
Florian Wagner <slvwagner@gmail.com>
2023-09-06 13:36:23 +0200

    Documentation update for data

--- R/data.R
+++ R/data.R
@@ -1,2 +1,35 @@
-#' ```{r child = "tbl_of_contents.Rmd.rda"}
-#' ```
+###################################################################
+#' Automatically creates a table of contents for a .Rmd file
+#' @name tbl_of_contents.Rmd
+#' @description
+#' R markdown date for
+#' @details
+#' The function argument is a stirng of a R markdown .Rmd file which can be loaded via \code{readLines("fileName.Rmd")}
+#' @export
+
+tbl_of_contents.Rmd <- function(){}
+
+#tbl_of_contents.Rmd <- readLines(paste0(getwd(),"/files/input/tbl_of_contents.Rmd"))
+#usethis:::use_data(tbl_of_contents.Rmd, internal = TRUE, overwrite = TRUE)
+
+###################################################################
+#' Automatically creates a table of contents for a .Rmd file
+#' @name test.Rmd
+#' @description
+#' R markdown data
+#' @details
+#' The function argument is a stirng of a R markdown .Rmd file which can be loaded via \code{readLines("fileName.Rmd")}
+#' @export
+#'
+test.Rmd <- function(){}
+
+###################################################################
+#' Automatically creates a table of contents for a .Rmd file
+#' @name test.html
+#' @description
+#' R markdown data
+#' @details
+#' The function argument is a stirng of a R markdown .Rmd file which can be loaded via \code{readLines("fileName.Rmd")}
+#' @export
+#'
+test.html <- function(){}
