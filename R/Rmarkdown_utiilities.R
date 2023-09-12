80b42132dff018eac130f6af5738357a329cb3b1
Florian Wagner <slvwagner@gmail.com>
2023-09-12 13:30:12 +0200

    Rework function

--- R/Rmarkdown_utiilities.R
+++ R/Rmarkdown_utiilities.R
@@ -24,17 +24,20 @@ r_toc_for_Rmd <- function(c_Rmd, create_nb = TRUE, create_top_link = TRUE , nb_f
   # create dataframe to work with
   p <- "^```"
 
-  df_data <- data.frame(index = 1:length(c_Rmd),
-                        c_Rmd,
-                        code_sections = lapply(c_Rmd, function(x) stringr::str_detect(x,p))|>unlist(),
-                        is.heading = stringr::str_detect(c_Rmd, "^#")
+  df_data <- data.frame(
+    index = 1:length(c_Rmd),
+    c_Rmd,
+    code_sections = lapply(c_Rmd, function(x)
+      stringr::str_detect(x, p)) |> unlist(),
+    is.heading = stringr::str_detect(c_Rmd, "^#")
   )
 
   # search and exclude code sections
   c_start_ii <- 0
   for (ii in 1:nrow(df_data)) {
     if (df_data$code_sections[ii] &  (c_start_ii != 0)) {
-      df_data$code_sections[c_start_ii:ii] <- rep(TRUE,length(c_start_ii:ii))
+      df_data$code_sections[c_start_ii:ii] <-
+        rep(TRUE, length(c_start_ii:ii))
       c_start_ii <- 0
     } else if (df_data$code_sections[ii]) {
       c_start_ii <- ii
@@ -46,108 +49,100 @@ r_toc_for_Rmd <- function(c_Rmd, create_nb = TRUE, create_top_link = TRUE , nb_f
 
   # Store headings
   # df_data$c_add <- rep("",nrow(df_data))
-  df_data$`#` <- stringr::str_detect(df_data$c_Rmd, "^#\\s")|>ifelse(1,0)
-  df_data$`##` <- stringr::str_detect(df_data$c_Rmd, "^##\\s")|>ifelse(1,0)
-  df_data$`###` <- stringr::str_detect(df_data$c_Rmd, "^###\\s")|>ifelse(1,0)
-  df_data$`####` <- stringr::str_detect(df_data$c_Rmd, "^####\\s")|>ifelse(1,0)
-  df_data$`#####` <- stringr::str_detect(df_data$c_Rmd, "^#####\\s")|>ifelse(1,0)
-  df_data$`######` <- stringr::str_detect(df_data$c_Rmd, "^######\\s")|>ifelse(1,0)
+  df_data$`#` <- stringr::str_detect(df_data$c_Rmd, "^#\\s") |> ifelse(1, 0)
+  df_data$`##` <-  stringr::str_detect(df_data$c_Rmd, "^##\\s") |> ifelse(1, 0)
+  df_data$`###` <- stringr::str_detect(df_data$c_Rmd, "^###\\s") |> ifelse(1, 0)
+  df_data$`####` <- stringr::str_detect(df_data$c_Rmd, "^####\\s") |> ifelse(1, 0)
+  df_data$`#####` <- stringr::str_detect(df_data$c_Rmd, "^#####\\s") |> ifelse(1, 0)
+  df_data$`######` <- stringr::str_detect(df_data$c_Rmd, "^######\\s") |> ifelse(1, 0)
 
   ##########################################################################
   # Headings
-  m <- df_data[df_data$is.heading,5:ncol(df_data)]
+  m <- df_data[df_data$is.heading, 5:ncol(df_data)]
 
   ##########################################################################
-  # correct structure
-  # check for headings in each column
-  is.heading <- apply(m, 2,function(x){
-    !sum(x)>0
-  })
+  # Analyze heading structure
+  heading_struct <- m|>
+    apply(2, function(x) {
+      sum(x)>0
+    })
 
-  # change structure so that the fist heading will be found in the first column
-  if(sum(is.heading)>1) {
-    for (ii in 1:length(is.heading)) {
-      if(!is.heading[ii]) {
-        push_front <- ii
-        # Add columns according to push_back
-        m_ <- switch (push_front,
-                      m_ = m,
-                      m_ = cbind(m[,2:6],p1=rep(0,nrow(m))),
-                      m_ = cbind(m[,2:6],p1=rep(0,nrow(m[,2:5])),p2=rep(0,nrow(m))),
-                      m_ = cbind(m[,3:6],p1=rep(0,nrow(m)),p2=rep(0,nrow(m)),p3=rep(0,nrow(m))),
-                      m_ = cbind(m[,4:6],p1=rep(0,nrow(m)),p2=rep(0,nrow(m)),p3=rep(0,nrow(m)),p4=rep(0,nrow(m))),
-                      m_ = cbind(m[,5:6],p1=rep(0,nrow(m)),p2=rep(0,nrow(m)),p3=rep(0,nrow(m)),p4=rep(0,nrow(m)),p5=rep(0,nrow(m)))
-        )
-        break
-      }
-    }
-  }else{
-    # Add columns according to push_back
-    push_back <- push_back
-    m_ <- switch (push_back,
-                  m_ = m,
-                  m_ = cbind(m,p1=rep(0,nrow(m))),
-                  m_ = cbind(m,p1=rep(0,nrow(m)),p2=rep(0,nrow(m))),
-                  m_ = cbind(m,p1=rep(0,nrow(m)),p2=rep(0,nrow(m)),p3=rep(0,nrow(m))),
-                  m_ = cbind(m,p1=rep(0,nrow(m)),p2=rep(0,nrow(m)),p3=rep(0,nrow(m)),p4=rep(0,nrow(m))),
-                  m_ = cbind(m,p1=rep(0,nrow(m)),p2=rep(0,nrow(m)),p3=rep(0,nrow(m)),p4=rep(0,nrow(m)),p5=rep(0,nrow(m))),
-                  m_ = cbind(m,p2=rep(0,nrow(m)),p2=rep(0,nrow(m)),p3=rep(0,nrow(m)),p4=rep(0,nrow(m)),p5=rep(0,nrow(m)),p6=rep(0,nrow(m)))
-    )
-  }
-
-  ##########################################################################
-  # find highest order Heading (Column)
-  highest_order_jj <- 0
-  for(ii in 1:6){
-    if(sum(m_[,ii]>0)){
+  # highest order heading column index
+  for (ii in 1:ncol(m)) {
+    if(heading_struct[ii]) {
       highest_order_jj <- ii
       break
     }
   }
 
-  # find index of highest order (row)
-  highest_order_ii <- 0
-  for(ii in 1:nrow(m)){
-    if(m_[ii,highest_order_jj] > 0){
+  # highest order heading row index
+  for (ii in 1:nrow(m)) {
+    if(m[ii,highest_order_jj]) {
       highest_order_ii <- ii
       break
     }
   }
 
-  # ##########################################################################
-  # # Correct heading structure
-  # shift all heading according to push_back and the first found highest order index
-  if (r_is.defined(push_back)) {
-    for (ii in 1:nrow(m_)) {
-      if (ii >= highest_order_ii) {
-        temp <- m[ii, ] |> as.matrix() |> as.vector()
-        if (push_back == 1) {
-          m_[ii, ] <- c(0, temp)
-        }
-        else if (push_back == 2) {
-          m_[ii, ] <- c(0, 0, temp)
-        }
-        else if (push_back == 3) {
-          m_[ii, ] <- c(0, 0, 0, temp)
-        }
-        else if (push_back == 4) {
-          m_[ii, ] <- c(0, 0, 0, 0, temp)
-        }
-        else if (push_back == 5) {
-          m_[ii, ] <- c(0, 0, 0, 0, 0, temp)
-        }
-        else if (push_back == 6) {
-          m_[ii, ] <- c(0, 0, 0, 0, 0, 0, temp)
-        }
-      }
+  # find first heading
+  for (ii in 1:6) {
+    if(m[1,ii]>0){
+      first_heading_column <- ii
     }
   }
 
   ##########################################################################
+  # correct heading structure
+  c_names <- c("#","##","###","####","#####","######")
+
+  if(highest_order_jj != first_heading_column){
+    # correct structure
+    temp <- m[1:(highest_order_ii-1),first_heading_column:6]
+    temp
+    temp <- switch (first_heading_column ,
+                    temp = temp,
+                    temp = cbind(temp,p1 = rep(0,nrow(temp))),
+                    temp = cbind(temp,p1 = rep(0,nrow(temp)),p2 = rep(0,nrow(temp))),
+                    temp = cbind(temp,p1 = rep(0,nrow(temp)),p2 = rep(0,nrow(temp)),p3 = rep(0,nrow(temp))),
+                    temp = cbind(temp,p1 = rep(0,nrow(temp)),p2 = rep(0,nrow(temp)),p3 = rep(0,nrow(temp)),p4 = rep(0,nrow(temp))),
+                    temp = cbind(temp,p1 = rep(0,nrow(temp)),p2 = rep(0,nrow(temp)),p3 = rep(0,nrow(temp)),p4 = rep(0,nrow(temp)),p5 = rep(0,nrow(temp))),
+                    temp = cbind(temp,p1 = rep(0,nrow(temp)),p2 = rep(0,nrow(temp)),p3 = rep(0,nrow(temp)),p4 = rep(0,nrow(temp)),p5 = rep(0,nrow(temp)),p6 = rep(0,nrow(temp))),
+    )
+    temp
+
+    temp1 <- m[highest_order_ii:nrow(m),highest_order_jj:6]
+    temp1 <- switch (highest_order_jj,
+                     temp1 = temp1,
+                     temp1 = cbind(temp1,p1 = rep(0,nrow(temp1))),
+                     temp1 = cbind(temp1,p1 = rep(0,nrow(temp1)),p2 = rep(0,nrow(temp1))),
+                     temp1 = cbind(temp1,p1 = rep(0,nrow(temp1)),p2 = rep(0,nrow(temp1)),p3 = rep(0,nrow(temp1))),
+                     temp1 = cbind(temp1,p1 = rep(0,nrow(temp1)),p2 = rep(0,nrow(temp1)),p3 = rep(0,nrow(temp1)),p4 = rep(0,nrow(temp1))),
+                     temp1 = cbind(temp1,p1 = rep(0,nrow(temp1)),p2 = rep(0,nrow(temp1)),p3 = rep(0,nrow(temp1)),p4 = rep(0,nrow(temp1)),p5 = rep(0,nrow(temp1))),
+                     temp1 = cbind(temp1,p1 = rep(0,nrow(temp1)),p2 = rep(0,nrow(temp1)),p3 = rep(0,nrow(temp1)),p4 = rep(0,nrow(temp1)),p5 = rep(0,nrow(temp1)),p6 = rep(0,nrow(temp1))),
+    )
+    names(temp1) <- c_names[highest_order_jj:6]
+    names(temp) <- c_names[highest_order_jj:6]
+    m_ <- rbind(temp,temp1)
+  }else{
+    if(highest_order_jj>0){ # remove not populated columns
+      m_ <- switch (highest_order_jj,
+                    m_ = m,
+                    m_ = cbind(m[,2:6],p1 = rep(0,nrow(m))),
+                    m_ = cbind(m[,3:6],p1 = rep(0,nrow(m)),p2 = rep(0,nrow(m))),
+                    m_ = cbind(m[,4:6],p1 = rep(0,nrow(m)),p2 = rep(0,nrow(m)),p3 = rep(0,nrow(m))),
+                    m_ = cbind(m[,5:6],p1 = rep(0,nrow(m)),p2 = rep(0,nrow(m)),p3 = rep(0,nrow(m)),p4 = rep(0,nrow(m))),
+                    m_ = cbind(m[,6:6],p1 = rep(0,nrow(m)),p2 = rep(0,nrow(m)),p3 = rep(0,nrow(m)),p4 = rep(0,nrow(m)),p5 = rep(0,nrow(m)))
+      )
+    }else{
+      m_ <- m
+    }
+  }
+  m_
+
+  ##########################################################################
   # create structure number system
   # Heading structure counts
-  heading_cnt <- rep(0,6)
-  heading_cnt_ <- rep(0,6)
+  heading_cnt <- rep(0, 6)
+  heading_cnt_ <- rep(0, 6)
   last_heading_edited <- 0
 
   # Structure string
@@ -159,64 +154,49 @@ r_toc_for_Rmd <- function(c_Rmd, create_nb = TRUE, create_top_link = TRUE , nb_f
              "                    + ")
 
   c_add_structure <- 1:nrow(m_)
-
+  column_cnt <- 0
   m__ <- m_
+  c_Heading_level <- 1:nrow(m_)
   for (ii in 1:nrow(m_)) {
-    for(jj in 1:6)
-      if(m_[ii, jj] > 0){
+    for (jj in 1:6)
+      if (m_[ii, jj] > 0) {
         heading_cnt[jj] <- heading_cnt[jj] + 1
-        if(last_heading_edited > jj) { # if heading order changes to higher order clear heading_cnt accordingly
-          heading_cnt[(jj+1):length(heading_cnt)] <- 0
+        if (last_heading_edited > jj) {
+          # if heading order changes to higher order clear heading_cnt accordingly
+          heading_cnt[(jj + 1):length(heading_cnt)] <- 0
+
         }
         last_heading_edited <- jj
         break
       }
-    m__[ii,] <- heading_cnt
+    m__[ii, 1:6] <- heading_cnt
     heading_cnt_ <- heading_cnt
-    c_add_structure[ii] <- c_add[jj] # need to offset toc
+    if(set_first_heading_level){
+      c_Heading_level[ii] <- c_names[jj]
+    }else{
+      c_Heading_level[ii] <- c_names[jj + (highest_order_jj-1)]
+    }
+    c_add_structure[ii] <- c_add[jj]
+
   }
 
   ##########################################################################
   # create structure number
-  c_nb <- m__|>
-    apply(1, function(x){
-      temp <- x[x>0]
+  c_nb <- m__ |>
+    apply(1, function(x) {
+      temp <- x[x > 0]
       paste0(temp, collapse = ".")
     })
 
   ##########################################################################
-  # create heading
-  c_Heading <- c_Rmd[df_data$is.heading]|> stringr::str_remove_all("#") |> stringr::str_trim()
-  c_Heading_level <- c_Rmd[df_data$is.heading] |> stringr::str_split(" ", simplify = TRUE)
-  c_Heading_level <- c_Heading_level[,1]
-
+  # create link link to table of contents
   c_toc_heading_string <- "Table of Contents"
-  c_top_link <- paste0("\n[",c_toc_heading_string,"](#",c_toc_heading_string,")\n")
+  c_top_link <-  paste0("\n[", c_toc_heading_string, "](#", c_toc_heading_string, ")\n")
+  c_top_link
 
-  if (set_first_heading_level & r_is.defined(push_front)) {
-    if((push_front-1) != 0){
-      c_Heading_level <- switch(
-        push_front-1,
-        c_Heading_level = c_Heading_level|>stringr::str_remove("#"),
-        c_Heading_level = c_Heading_level|>stringr::str_remove("##"),
-        c_Heading_level = c_Heading_level|>stringr::str_remove("###"),
-        c_Heading_level = c_Heading_level|>stringr::str_remove("####"),
-        c_Heading_level = c_Heading_level|>stringr::str_remove("#####"),
-        c_Heading_level = c_Heading_level|>stringr::str_remove("######"),
-      )
-    }
-    c_toc_heading <- paste0("# ")
-  } else {
-    c_toc_heading <- switch(
-      push_front,
-      c_toc_heading = paste0("# "),
-      c_toc_heading = paste0("## "),
-      c_toc_heading = paste0("### "),
-      c_toc_heading = paste0("#### "),
-      c_toc_heading = paste0("##### "),
-      c_toc_heading = paste0("###### ")
-    )
-  }
+  ##########################################################################
+  c_Heading <- c_Rmd[df_data$is.heading]|>stringr::str_remove_all("#")|>stringr::str_trim()
+  c_Heading
 
   ##########################################################################
   # create anchor
@@ -254,12 +234,39 @@ r_toc_for_Rmd <- function(c_Rmd, create_nb = TRUE, create_top_link = TRUE , nb_f
     c_toc <- paste0("[", c_Heading, "](#A_", c_Heading, ")")
   }
 
-  # offset toc according to heading structure
+  # format toc according to found heading structure
   c_toc <- paste0(c_add_structure, c_toc)
 
   #########################################################################
+  # Enhance headings
+  df_data_ <- dplyr::left_join(df_data[, 1:4],
+                               data.frame(index = rownames(m__) |> as.integer(),
+                                          c_anchor),
+                               by = "index")
+
+  df_data_$c_Rmd_ <-  ifelse(!is.na(df_data_$c_anchor), df_data_$c_anchor, c_Rmd)
+
+
+
+  #########################################################################
+  # create TOC
+  highest_order_jj <- ifelse(set_first_heading_level, 1, highest_order_jj)
+  c_toc_link <- switch(highest_order_jj,
+                       paste0(c_names[1]," ",c_toc_heading_string),
+                       paste0(c_names[2]," ",c_toc_heading_string),
+                       paste0(c_names[3]," ",c_toc_heading_string),
+                       paste0(c_names[4]," ",c_toc_heading_string),
+                       paste0(c_names[5]," ",c_toc_heading_string),
+                       paste0(c_names[6]," ",c_toc_heading_string)
+  )
+
+  c_toc_link <- ifelse(create_top_link,
+                       paste0(c_toc_link, "<a name=\"", c_toc_heading_string, "\"></a>"),
+                       c_toc_link)
+
+  #########################################################################
   # find position to insert table of contents
-  check <- stringr::str_detect(c_Rmd, "(?:^#\\s|^##\\s)")
+  check <- stringr::str_detect(c_Rmd, "```")
 
   for (ii in 1:length(c_Rmd)) {
     if (check[ii]) {
@@ -269,38 +276,13 @@ r_toc_for_Rmd <- function(c_Rmd, create_nb = TRUE, create_top_link = TRUE , nb_f
   }
 
   #########################################################################
-  # Enhance headings
-  df_data_ <- dplyr::left_join(df_data[,1:4],
-                               data.frame(index = rownames(m__)|>as.integer(),
-                                          c_anchor),
-                               by = "index")
-
-  df_data_$c_Rmd_ <- ifelse(df_data_$is.heading,df_data_$c_anchor, c_Rmd)
-
-  ##########################################################################
-  # create create top link
-  c_toc_heading <- ifelse(create_top_link,
-                          paste0("\n",c_toc_heading," ", c_toc_heading_string,"<a name=\"",c_toc_heading_string,"\"></a>"),
-                          paste0(c_toc_heading," ", c_toc_heading_string))
-
-
-  #########################################################################
   # Insert table of contents
-  if (create_nb) {
-    # insert table off contents
-    c_Rmd <- c(df_data_$c_Rmd_ [1:(c_start - 1)],
-               c_toc_heading,
-               c_toc,
-               "\n",
-               df_data_$c_Rmd_[c_start:nrow(df_data)])
-    return(c_Rmd)
-  } else{
-    # insert table off contents
-    c_Rmd <- c(df_data_$c_Rmd_[1:(c_start - 1)],
-               c_toc_heading,
-               c_toc,
-               "\n",
-               df_data_$c_Rmd_[c_start:nrow(df_data)])
-    return(c_Rmd)
-  }
+  # insert table off contents
+  c_Rmd <- c(df_data_$c_Rmd_ [1:(c_start - 1)],
+             c_toc_link,
+             c_toc,
+             "\n",
+             df_data_$c_Rmd_[c_start:nrow(df_data)]
+  )
+  return(c_Rmd)
 }
