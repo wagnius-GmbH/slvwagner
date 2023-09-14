19b573cf22cf5a75d0d278655e959e0f798a173d
Florian Wagner <slvwagner@gmail.com>
2023-09-14 17:49:01 +0200

    update documentation: tbl_of_contents.Rmd

--- R/data.R
+++ R/data.R
@@ -4,7 +4,7 @@
 #' @description
 #' Data for r_tbl_contents
 #' @export
-tbl_of_contents.Rmd <- function(){}
+"tbl_of_contents.Rmd"
 
 # tbl_of_contents.Rmd <- readLines(paste0(getwd(),"/files/input/tbl_of_contents.Rmd"))
 # usethis:::use_data(tbl_of_contents.Rmd, internal = TRUE, overwrite = TRUE)
