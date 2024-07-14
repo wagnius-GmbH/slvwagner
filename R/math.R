e5e3192748c57b7e1ed9f849743eabdedef38f57
Florian Wagner <53572950+slvwagner@users.noreply.github.com>
2024-07-14 18:00:29 +0200

    math_circle_from3points: update error msg

--- R/math.R
+++ R/math.R
@@ -111,7 +111,7 @@ math_circle_from3points<-function(x){
                       radius  = sqrt((-c_result[2]/2)^2+(-c_result[3]/2)^2- c_result[1]))
     )
   }else{
-    return(writeLines(paste("only matrix[3][2] (3 points with 2 coordinates) can be used to calculat the circle")))
+    return(writeLines(paste("only matrix[3][2] (3 points with 2 coordinates) can be used to calculate a circle")))
   }
 }
 
